8efb260717364fda5f4272dde2e61dbd3d8814e9
sanstwy777 <sanstwy777@outlook.com>
2020-10-19 12:57:23 +0800

    implement article editor with react-draft-wysiwyg

--- react-dashboard-starter/src/views/Article/Edit.js
+++ react-dashboard-starter/src/views/Article/Edit.js
@@ -7,23 +7,63 @@ import {
     Input,
     DatePicker
 } from 'antd'
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import draftToHtml from 'draftjs-to-html';
 
 const formItemLayout = {
     labelCol: { span: 2 },
     wrapperCol: { span: 18 },
-  };
+};
 
 const formTailLayout = {
     labelCol: { span: 2 },
     wrapperCol: { span: 18, offset: 2 },
-  };
+};
 
 class Edit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contentState : null,
+            contentValid: null
+        };
+    }
+    
+    checkContent = (rule, value) => {
+        if (!this.emptyContent()) {
+            this.setState({
+                contentValid: true
+            })
+            return Promise.resolve();
+        }
+        
+        this.setState({
+            contentValid: false
+        })
+        return Promise.reject('Content is required!');
+    };
+
+    emptyContent() {
+        if (draftToHtml(this.state.contentState).length > 0) {
+            return false;
+        }
+        return true;
+    }
+
+    onContentStateChange = (contentState) => {
+        this.setState({
+            contentState: draftToHtml(contentState).trim() === "<p></p>" ? null : contentState,
+        });
+        // console.log('as HTML:', draftToHtml(contentState));
+    };
+
     onFinish = values => {
-        // console.log('Received values of form: ', values);
+        console.log('Received values of form: ', values);
     };
 
     render() {
+        const { contentState, contentValid } = this.state
         return (
             <Card 
                 title="Article Edit" 
@@ -71,9 +111,22 @@ class Edit extends Component {
                         {...formItemLayout}
                         label="Content"
                         name="content"
-                        rules={[{ required: true, message: 'Content is required!' }]}
+                        rules={[
+                            // { required: true, message: 'Content is required!' },
+                            { validator: this.checkContent }
+                        ]}
                     >
-                        <div>Content</div>
+                        <Editor
+                            wrapperClassName="demo-wrapper"
+                            editorClassName="demo-editor"
+                            editorContent={contentState}
+                            onContentStateChange={this.onContentStateChange}
+                            editorStyle={{
+                                border: `1px solid ${contentValid || contentValid== null ? "" : "#FF0000"}`,
+                                minHeight: "10em",
+                                lineHeight: '75%'
+                            }}
+                        />
                     </Form.Item>
                     <Form.Item
                         {...formTailLayout}
