4b607def0d170cb5aa2d18ab2cd1dab662cf4438
sanstwy777 <sanstwy777@outlook.com>
2020-10-20 13:29:46 +0800

    add login layout

--- react-dashboard-starter/src/views/Login/index.js
+++ react-dashboard-starter/src/views/Login/index.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react'
+import { Form, Input, Button, Checkbox, Card } from 'antd';
+import './login.less'
+
+const layout = {
+    labelCol: { span: 4 },
+    wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+    wrapperCol: { offset: 4, span: 16 },
+};
 
 export default class Login extends Component {
+
+    onFinish = values => {
+        console.log('Success:', values);
+    };
+    
+    onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
+
     render() {
         return (
-            <div>
-                Login
-            </div>
+            <Card title="Login" className="sans-login-wrapper">
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={this.onFinish}
+                    onFinishFailed={this.onFinishFailed}
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+            
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+            
+                    <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+            
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
         )
     }
 }
